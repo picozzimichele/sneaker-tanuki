82a9441c4d51e097aa43c45c4e3081ada165184f
Michele Picozzi <michele.picozzi@hotmail.it>
2023-11-20 18:18:07 +0900

    filtered data and quantity

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -5,16 +5,24 @@ import { getValuesAndQuantities } from "@/utils/helperFunctions";
 import { useContext, useEffect, useState } from "react";
 
 export default function CheckoutPage() {
-    const { selectedProducts } = useContext(ProductContext);
-    const [mounted, setMounted] = useState(false);
-    console.log(selectedProducts);
     // In a real case scenario with database the call here should be handled by the server component rather than here
     // Since we do not have an actual database call, but only limited dummy data this will do for testing purposes with filtering on the client side that has access to local storage and context
     // fetch products based on category and sort params
+    const { selectedProducts } = useContext(ProductContext);
+    const [mounted, setMounted] = useState(false);
 
     const quantityAndProducts = getValuesAndQuantities(selectedProducts);
-    console.log(quantityAndProducts);
     const productDetails = dummyDateJSON;
+    const filteredData = productDetails.products.filter((item) =>
+        selectedProducts.includes(item.id.toString())
+    );
+
+    const filteredDataWithQuantity = filteredData.map((item) => {
+        const quantity = quantityAndProducts.find(
+            (product) => product.value === item.id.toString()
+        );
+        return { ...item, quantity: quantity?.quantity };
+    });
 
     useEffect(() => {
         setMounted(true);
@@ -24,9 +32,9 @@ export default function CheckoutPage() {
         // this solves hydratation issue with context, we want to make sure that the context and local storage is available before rendering the page
         mounted && (
             <div className="py-16">
-                {quantityAndProducts?.map((item, index) => (
+                {filteredDataWithQuantity?.map((item, index) => (
                     <div key={index}>
-                        {item.value} {item.quantity}
+                        {item.name} {item.quantity}
                     </div>
                 ))}
             </div>
