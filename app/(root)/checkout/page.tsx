41fce95c10bddb57b7f1548bc8f8e52ab284032a
Michele Picozzi <michele.picozzi@hotmail.it>
2023-11-20 18:48:00 +0900

    removes one product at the time

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -9,7 +9,7 @@ export default function CheckoutPage() {
     // In a real case scenario with database the call here should be handled by the server component rather than here
     // Since we do not have an actual database call, but only limited dummy data this will do for testing purposes with filtering on the client side that has access to local storage and context
     // fetch products based on category and sort params
-    const { selectedProducts } = useContext(ProductContext);
+    const { selectedProducts, setSelectedProducts } = useContext(ProductContext);
     const [mounted, setMounted] = useState(false);
 
     const quantityAndProducts = getValuesAndQuantities(selectedProducts);
@@ -31,6 +31,13 @@ export default function CheckoutPage() {
         0
     );
 
+    const removeProductFromCart = (productId: string) => {
+        const index = selectedProducts.indexOf(productId);
+        if (index > -1) {
+            setSelectedProducts((prev: []) => prev.splice(index, 1));
+        }
+    };
+
     useEffect(() => {
         setMounted(true);
     }, []);
@@ -38,9 +45,9 @@ export default function CheckoutPage() {
     return (
         // this solves hydratation issue with context, we want to make sure that the context and local storage is available before rendering the page
         mounted && (
-            <div className="py-16 flex flex-col w-full">
+            <div className="py-16 flex flex-col w-full gap-10">
                 {filteredDataWithQuantity?.map((item, index) => (
-                    <div key={index} className="flex w-full h-40 bg-green-400">
+                    <div key={index} className="flex w-full h-40">
                         <div className="bg-gray-200 flex aspect-square">
                             <ProductCard
                                 id={item.id.toString()}
@@ -50,6 +57,18 @@ export default function CheckoutPage() {
                                 brand={item.brand}
                                 category={item.category}
                             />
+                        </div>
+                        <div className="flex flex-col justify-center gap-2 px-5">
+                            <div>Quantity: {item.quantity}</div>
+                            <div>Price: $ {item.price}</div>
+                            <button
+                                onClick={() => {
+                                    removeProductFromCart(item.id.toString());
+                                }}
+                                className="bg-red-600 hover:bg-red-700 text-white px-3 py-2 rounded-md"
+                            >
+                                Remove
+                            </button>
                         </div>
                     </div>
                 ))}
